aa3977e887ae5556781b0d487cf32cd4a28923f6
J4F3ET <jslopezz@udistrital.edu.co>
2024-04-28 19:15:24 -0500

    Actualizar astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: "https://j4f3et.github.io/J4F3ET/",
-    base: "/J4F3ET/",
+    base: "/J4F3ET/*",
 });
